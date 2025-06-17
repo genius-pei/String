99d102771e4a5b0758c3e0a580cce77627b5a343
Yiming <2018565194@qq.com>
2025-06-17 19:22:13 +0800

    Update String.h

--- String.h
+++ String.h
@@ -1,13 +1,19 @@
 #pragma once
 #include<iostream>
 #include<string.h>
+#include<assert.h>
 namespace yiming
 {
 	class string
 	{
 	public:
 		string();
-		string(const char* str);
+		string(const char* str=" ");
+		const char* c_str()const;
+		~string();
+		size_t size();
+		char& operator[](size_t i)const;
+		//const char& string::operator[](size_t i)const;
 	private:
 		char* _str;
 		size_t _size;
