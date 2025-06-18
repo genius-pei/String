85e2ab3d65f2885ebd151119fb80434b30aa7ddf
Yiming <2018565194@qq.com>
2025-06-18 16:56:25 +0800

    Update String.h

--- String.h
+++ String.h
@@ -7,13 +7,22 @@ namespace yiming
 	class string
 	{
 	public:
+		typedef char* iterator;
+		typedef const char* const_iterator;
 		string();
 		string(const char* str=" ");
 		const char* c_str()const;
 		~string();
 		size_t size();
-		char& operator[](size_t i)const;
-		//const char& string::operator[](size_t i)const;
+		iterator begin();
+		iterator end();
+		const_iterator begin()const;
+		const_iterator end()const;
+		char& operator[](size_t i);
+		const char& operator[](size_t i)const;
+		void push_back(char ch);
+		void append(const char* str);
+		void reserve(size_t n);
 	private:
 		char* _str;
 		size_t _size;
