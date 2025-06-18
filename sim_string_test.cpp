0fe258b85e64fa2daef7f4c4e209b3220505d52a
Yiming <2018565194@qq.com>
2025-06-18 20:18:23 +0800

    Update sim_string_test.cpp

--- sim_string_test.cpp
+++ sim_string_test.cpp
@@ -5,14 +5,25 @@ namespace yiming {
 	void test01()
 	{
 		string s1("hello world");
-		s1.push_back('y');
+		s1 += 'y';
 		cout << s1 << endl;
 
+	}
+	void test02()
+	{
+		string s1("hello world");
+		s1.insert(5, 'm');
+		cout << s1 << endl;
+		string s2("hello world");
+		s2.insert(5, "my");
+		cout << s2 << endl;
 	}
 	
 }
 int main()
 {
-	yiming::test01();
+	//yiming::test01();
+	yiming::test02();
+
 	return 0;
 }
