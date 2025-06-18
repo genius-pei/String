70c41ac3576245dbc5e7936e6dfceb2067e590f4
Yiming <2018565194@qq.com>
2025-06-18 16:56:44 +0800

    Update sim_string_test.cpp

--- sim_string_test.cpp
+++ sim_string_test.cpp
@@ -1,1 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include"String.h"
+
+
+using namespace std;
+namespace yiming {
+
+	void test01()
+	{
+		string s1("hello world");
+		s1.push_back('y');
+		cout << s1 << endl;
+
+	}
+	int main()
+	{
+
+	}
+}
